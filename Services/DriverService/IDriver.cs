5c9a84f80df45892924bdeb8fed6ea87f60a5747
Nastaran Malekshahi <Nasi_Mlk@outlook.com>
2025-06-15 00:07:27 +0200

    Added NUnit moq test for hunger

---
+++ Services/DriverService/IDriver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DriverService
+{
+    public interface IDriver
+    {
+        int Hunger { get; set; }
+        void IncreaseHunger();
+        void Eat();
+        HungerLevel GetHungerLevel();
+    }
+
+    public enum HungerLevel
+    {
+        Full,
+        Hungry,
+        Starving
+    }
+}
